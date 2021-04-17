1a36429db09bd91daec3c1b4d8907324633939d4
Ashwin R <vivekashwin@gmail.com>
2021-04-18 00:58:24 +0530

    Added files

---
+++ config/webpack.common.js
@@ -0,0 +1,7 @@
+const srcPath = path.join(__dirname, '../src');
+const publicPath = '/';
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index'],
+
+};
---
+++ config/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = (env) => {
+    if (env.NODE_ENV === 'production') {
+      process.env.NODE_ENV = env.NODE_ENV;
+      return require('./webpack.production');
+    }
+    return require('./webpack.local');
+};
+  
