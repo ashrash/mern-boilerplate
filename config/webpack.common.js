ff718326d9037cf76d6e38433b54cf1b6cf5f02b
Ashwin R <vivekashwin@gmail.com>
2021-04-20 21:37:12 +0530

    Added new files

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,6 +2,20 @@ const srcPath = path.join(__dirname, '../src');
 const publicPath = '/';
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index'],
-
+    entry: ['./src/index'],
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            enforce: 'pre',
+            include: srcPath,
+            loader: 'eslint-loader',
+        },
+        {
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+          }
+        ]
+    }
 };
